8b5f05f3d07529b18a8302aefffed030e71af275
Takayuki Maruyama <bis5.wsys@gmail.com>
2024-12-08 16:40:20 +0900

    distance precalc before insert record

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -111,11 +111,21 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 	}
 	defer tx.Rollback()
 
+	lastLocation := &ChairLocation{}
+	distance := 0
+	if err := tx.GetContext(ctx, lastLocation, `SELECT * FROM chair_locations WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1`, chair.ID); err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+		distance = abs(req.Latitude-lastLocation.Latitude) + abs(req.Longitude-lastLocation.Longitude)
+	}
+
 	chairLocationID := ulid.Make().String()
 	if _, err := tx.ExecContext(
 		ctx,
-		`INSERT INTO chair_locations (id, chair_id, latitude, longitude) VALUES (?, ?, ?, ?)`,
-		chairLocationID, chair.ID, req.Latitude, req.Longitude,
+		`INSERT INTO chair_locations (id, chair_id, latitude, longitude, distance) VALUES (?, ?, ?, ?, ?)`,
+		chairLocationID, chair.ID, req.Latitude, req.Longitude, distance,
 	); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
@@ -125,19 +135,6 @@ func chairPostCoordinate(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, location, `SELECT * FROM chair_locations WHERE id = ?`, chairLocationID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
-	}
-
-	lastLocation := &ChairLocation{}
-	if err := tx.GetContext(ctx, lastLocation, `SELECT * FROM chair_locations WHERE id != ? AND chair_id = ? ORDER BY created_at DESC LIMIT 1`, chairLocationID, chair.ID); err != nil {
-		if !errors.Is(err, sql.ErrNoRows) {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-		distance := abs(location.Latitude-lastLocation.Latitude) + abs(location.Longitude-lastLocation.Longitude)
-		if _, err := tx.ExecContext(ctx, "UPDATE chair_locations SET distance = ? WHERE id = ?", distance, chairLocationID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
 	}
 
 	ride := &Ride{}
